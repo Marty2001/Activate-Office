52ccce0ebb0faa9f59b53622e715de891aae5d1b
Marty <92310937+Marty2001@users.noreply.github.com>
2025-10-21 15:12:34 +0800

    Update URLss

--- ohook-activation-wrapper/Ohook-Activation-Wrapper.ps1
+++ ohook-activation-wrapper/Ohook-Activation-Wrapper.ps1
@@ -52,7 +52,7 @@ if (-not $args) {
     try { [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 } catch {}
 
     $URLs = @(
-        'https://raw.githubusercontent.com/yourusername/ohook-activation-aio/main/Ohook_Activation_AIO.cmd',
+        'https://raw.githubusercontent.com/Marty2001/Activate-Office/refs/heads/main/ohook-activation-wrapper/Ohook_Activation_AIO.cmd',
         'https://github.com/Marty2001/Activate-Office/blob/main/ohook-activation-wrapper/Ohook_Activation_AIO.cmd'
     )
     Write-Progress -Activity "Downloading..." -Status "Please wait"
