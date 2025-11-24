a2bcc792980624e0ab551b244ccc0bbf38ec3dad
Marty <92310937+Marty2001@users.noreply.github.com>
2025-11-24 16:41:28 +0800

    Update URL for office activation script

--- BitCourse-Office-Activation.ps1
+++ BitCourse-Office-Activation.ps1
@@ -52,7 +52,7 @@ Write-Host ''
     try { [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 } catch {}
 
     $URLs = @(
-        'http://raw.githubusercontent.com/Marty2001/Activate-Office/refs/heads/main/BitCourse_Office_Activation%203.8.cmd'
+        'https://raw.githubusercontent.com/Marty2001/Activate-Office/refs/heads/main/BitCourse_OfficeActivation%203.8.cmd'
     )
     
     Write-Progress -Activity "Downloading..." -Status "Please wait"
